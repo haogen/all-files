3e687bcb273ad184bbcaa2b219dc4ef429b94259
“CastroGenelyn” <genelync39@gmail.com>
2021-06-04 09:28:28 -0700

    Add Summary and category

--- Day 13/reports/routes/web.php
+++ Day 13/reports/routes/web.php
@@ -22,3 +22,43 @@ Route::get('/orders',[
     App\Http\Controllers\ReportingController::class,
     'orders'
 ]);
+
+Route::get('/summary',[
+    App\Http\Controllers\ReportingController::class,
+    'summary'
+]);
+
+Route::get('/category/toys',[
+    App\Http\Controllers\ReportingController::class,
+    'category_toys'
+]);
+
+Route::get('/category/books',[
+    App\Http\Controllers\ReportingController::class,
+    'category_books'
+]);
+
+Route::get('/category/homesup',[
+    App\Http\Controllers\ReportingController::class,
+    'category_homesup'
+]);
+
+Route::get('/category/accessories',[
+    App\Http\Controllers\ReportingController::class,
+    'category_accessories'
+]);
+
+Route::get('/category/gadgets',[
+    App\Http\Controllers\ReportingController::class,
+    'category_gadgets'
+]);
+
+Route::get('/category/food',[
+    App\Http\Controllers\ReportingController::class,
+    'category_food'
+]);
+
+Route::get('/category/appliances',[
+    App\Http\Controllers\ReportingController::class,
+    'category_appliances'
+]);
